06ba1ae2a6c4499060b8aabc428be549f9ad3b79
solaguirre <68126593+solaguirre@users.noreply.github.com>
2020-10-14 15:53:09 -0700

    Added questions to array and package.json as well as .gitignore.

---
+++ Develop/index.js
@@ -0,0 +1,60 @@
+// array of questions for user
+const questions = [
+    {
+        type: "input",
+        message: "What is the name of your project?",
+        name: "title"
+      },
+      {
+        type: "input",
+        message: "Plese write a description of your project:",
+        name: "description"
+      },
+      {
+        type: "input",
+        message: "Please type you Table of Contents:",
+        name: "contents"
+      },
+      {
+        type: "input",
+        message: "Please describe the project's installation",
+        name: "installation"
+      },
+      {
+        type: "input",
+        message: "Please describe the projects usage:",
+        name: "usage"
+      },
+      {
+        type: "input",
+        message: "License",
+        name: "license"
+      },
+      {
+        type: "input",
+        message: "Please input the information for the project's contributors",
+        name: "contributors"
+      },
+      {
+        type: "input",
+        message: "What tests did you create for this project?",
+        name: "tests"
+      },
+      {
+        type: "input",
+        message: "Questions",
+        name: "questions"
+      },
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
