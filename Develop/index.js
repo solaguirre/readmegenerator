c76c55971b9d2d50b8decca4d8c3c78850cf1516
solaguirre <68126593+solaguirre@users.noreply.github.com>
2020-10-14 18:28:17 -0700

    Updated License choices for README section on license. Added function to write README file.

--- Develop/index.js
+++ Develop/index.js
@@ -1,58 +1,74 @@
 // array of questions for user
+const inquirer = require('inquirer');
+const fs =  require('fs');
+
+const generateMarkdown = require('generateMarkdown.js');
+
 const questions = [
+  {
+      type: "input",
+      message: "What is the name of your project?",
+      name: "title"
+    },
     {
-        type: "input",
-        message: "What is the name of your project?",
-        name: "title"
-      },
-      {
-        type: "input",
-        message: "Plese write a description of your project:",
-        name: "description"
-      },
-      {
-        type: "input",
-        message: "Please type you Table of Contents:",
-        name: "contents"
-      },
-      {
-        type: "input",
-        message: "Please describe the project's installation",
-        name: "installation"
-      },
-      {
-        type: "input",
-        message: "Please describe the projects usage:",
-        name: "usage"
-      },
-      {
-        type: "input",
-        message: "License",
-        name: "license"
-      },
-      {
-        type: "input",
-        message: "Please input the information for the project's contributors",
-        name: "contributors"
-      },
-      {
-        type: "input",
-        message: "What tests did you create for this project?",
-        name: "tests"
-      },
-      {
-        type: "input",
-        message: "Questions",
-        name: "questions"
-      },
+      type: "input",
+      message: "Plese write a description of your project:",
+      name: "description"
+    },
+    {
+      type: "input",
+      message: "Please type you Table of Contents:",
+      name: "contents"
+    },
+    {
+      type: "input",
+      message: "Please describe the project's installation process if there is one:",
+      name: "installation"
+    },
+    {
+      type: "input",
+      message: "Please provide instructions for the project's usage:",
+      name: "usage"
+    },
+    {
+      type: "list",
+      message: "Choose a license for your project:",
+      choices: ['MIT License', 'GNU AGPLv3', 'GNU GPLv3','Mozilla Public License 2.0', 'Apache License 2.0', 'Boost Software License 1.0', 'The Unlicense'],
+      name: "license"
+    },
+    {
+      type: "input",
+      message: "Please input the information for the project's contributors",
+      name: "contributors"
+    },
+    {
+      type: "input",
+      message: "If any, what tests did you create for this project? How do you run them?",
+      name: "tests"
+    },
+    {
+      type: "input",
+      message: "Questions",
+      name: "questions"
+    },
 ];
+
+
 
 // function to write README file
 function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, err => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Sucess! Your README.md file has been generated")
+  });
+
+  let filename = data.name
 }
 
 // function to initialize program
-function init() {
+async function init() {
 
 }
 
